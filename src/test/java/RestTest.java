f0a2668d5022fe0e4b9c35947a654f7da3f72697
Leonardo Venegas <lvenegas.connectis@banchochile.cl>
2022-07-05 11:11:11 -0400

    creando metodo setup

--- src/test/java/RestTest.java
+++ src/test/java/RestTest.java
@@ -1,38 +1,43 @@
 import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.equalTo;
 
 public class RestTest {
 
+    @BeforeTest
+    public void setUp() {
+        RestAssured.baseURI = "https://reqres.in";
+        RestAssured.basePath = "/api";
+        RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
+    }
+
     @Test
-    public void loginTest(){
-        String response = RestAssured
-                .given()
-                .log().all()
+    public void loginTest() {
+        given()
                 .contentType(ContentType.JSON)
                 .body("{\n" +
                         "    \"email\": \"eve.holt@reqres.in\",\n" +
                         "    \"password\": \"cityslicka\"\n" +
                         "}")
-                .post("https://reqres.in/api/login")
+                .post("login")
                 .then()
-                .log().all()
-                .extract()
-                .asString();
+                .statusCode(200)
+                .body("token", notNullValue());
     }
 
     @Test
-    public void getUsers(){
-        RestAssured
-                .given()
-                .log().all()
+    public void getUsers() {
+        given()
                 .contentType(ContentType.JSON)
-                .get("https://reqres.in/api/users/2")
+                .get("users/2")
                 .then()
-                .log().all()
                 .statusCode(200)
                 .body("data.id", notNullValue())
                 .body("data.id", equalTo(2));
