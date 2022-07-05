219ea3397abc7b916221edda78d722e3cd8d0f8a
Leonardo Venegas <lvenegas.connectis@banchochile.cl>
2022-07-05 16:09:34 -0400

    Metodos delete, put y patch

--- src/test/java/RestTest.java
+++ src/test/java/RestTest.java
@@ -1,13 +1,17 @@
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpStatus;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class RestTest {
 
@@ -16,30 +20,71 @@ public class RestTest {
         RestAssured.baseURI = "https://reqres.in";
         RestAssured.basePath = "/api";
         RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
+        RestAssured.requestSpecification = new RequestSpecBuilder()
+                .setContentType(ContentType.JSON)
+                .build();
     }
 
     @Test
     public void loginTest() {
         given()
-                .contentType(ContentType.JSON)
                 .body("{\n" +
                         "    \"email\": \"eve.holt@reqres.in\",\n" +
                         "    \"password\": \"cityslicka\"\n" +
                         "}")
                 .post("login")
                 .then()
-                .statusCode(200)
+                .statusCode(HttpStatus.SC_OK)
                 .body("token", notNullValue());
     }
 
     @Test
     public void getUsers() {
         given()
-                .contentType(ContentType.JSON)
                 .get("users/2")
                 .then()
-                .statusCode(200)
+                .statusCode(HttpStatus.SC_OK)
                 .body("data.id", notNullValue())
                 .body("data.id", equalTo(2));
+    }
+
+    @Test
+    public void deleteTest(){
+        given()
+                .delete("users/2")
+                .then()
+                .statusCode(HttpStatus.SC_NO_CONTENT);
+    }
+
+    @Test
+    public void putTest(){
+        String nombre = given()
+                .when()
+                .body("{\n" +
+                        "    \"name\": \"morpheus\",\n" +
+                        "    \"job\": \"zion resident\"\n" +
+                        "}")
+                .put("users/2")
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .extract()
+                .jsonPath().getString("name");
+        assertThat(nombre, equalTo("morpheus"));
+    }
+
+    @Test
+    public void patchTest(){
+        String trabajo = given()
+                .when()
+                .body("{\n" +
+                        "    \"name\": \"morpheus\",\n" +
+                        "    \"job\": \"zion resident\"\n" +
+                        "}")
+                .patch("users/2")
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .extract()
+                .jsonPath().getString("job");
+        assertThat(trabajo, equalTo("zion resident"));
     }
 }
